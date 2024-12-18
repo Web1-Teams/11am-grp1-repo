9166e6be2c5678f5fbc909e0eacd41c1a7855b3b
kareem-halaweh <s12327129@stu.najah.edu>
2024-12-18 22:52:29 +0200

    first push

---
+++ aura/src/App.js
@@ -0,0 +1,29 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Shop from "./pages/shop";
+import Main from "./pages/main";
+import Cart from "./pages/cart";
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/home" element={<Main />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/community" element={<Shop />} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/about" element={<Shop />} />
+          <Route path="/upload" element={<Shop />} />
+          <Route path="/profile" element={<Shop />} />
+          <Route path="/checkout" element={<Shop />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
