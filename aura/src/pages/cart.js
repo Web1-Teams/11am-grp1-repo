20c584606f57fe87a0c37e74b772dd10160ccc5a
kareem-halaweh <s12327129@stu.najah.edu>
2024-12-19 21:44:17 +0200

    added the menue bar + edited on the cart page + added the components on the main page

--- aura/src/pages/cart.js
+++ aura/src/pages/cart.js
@@ -1,15 +1,15 @@
-
 import CartItems from "../components/cartItems.js";
 import Cartsummry from "../components/cartsummry.js";
 import Secnavbar from "../components/secnavbar.js";
 import "./styles/cart-style.css"
 
-const Cart = () => {
+const Cart = ({ cartItems, setCartItems }) => {
+  console.log("Cart Items in cart: ", cartItems);
   return (
-    <div className="cart-container"> 
-      <Secnavbar/>
-      <CartItems />
-      <Cartsummry />
+    <div className="cart-container">
+      <Secnavbar />
+      <CartItems cartItems={cartItems} setCartItems={setCartItems} />
+      <Cartsummry cartItems={cartItems} />
     </div>
   );
 }
