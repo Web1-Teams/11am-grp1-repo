ba51ec940bde3b80eed776d0002d0098f40b8507
Omar M. Hammami <s12323569@stu.najah.edu>
2025-01-03 19:02:47 +0200

    testing

--- aura/src/components/ChatBoxCommunity.js
+++ aura/src/components/ChatBoxCommunity.js
@@ -9,14 +9,22 @@ const ChatBoxCommunity = ({ activeUser, messages }) => {
             <img src={activeUser.avatar} alt={activeUser.name} className="user-avatar-chat" />
             <span className="Chat-UserName">{activeUser.name}</span>
           </div>
+
+          {}
+          {activeUser.messages.length > 0 && (
+            <div className="user-message-in-chat">
+              {activeUser.messages[0]} {}
+            </div>
+          )}
+
           <div className="messages">
             {messages.map((message, index) => (
               <div 
-                key={index} 
-                className={`message-item ${message.isUserMessage ? 'user-message' : 'other-message'}`}
-              >
-                {message.text}
-              </div>
+              key={index} 
+              className={`message-item ${message.isUserMessage ? 'user-message' : 'other-message'}`}
+            >
+              {message.text}
+            </div>
             ))}
           </div>
         </>
--- aura/src/pages/community.js
+++ aura/src/pages/community.js
@@ -6,12 +6,11 @@ import '../pages/styles/Community.css';
 
 const Community = () => {
   const [users, setUsers] = useState([
-    { id: 1, name: 'Maggie Smith', avatar: 'https://www.palestineremembered.com/Jerusalem/GeoPoint/Dome_Of_The_Rock-10150.jpg', messages: ['Hey, I’m interested in your work'] },
-    { id: 2, name: 'Emma Watson', avatar: 'https://image.made-in-china.com/2f0j00QWagThVImqzc/Birthday-Wedding-Party-Club-Promotion-Gift-Flower-Red-Fresh-Cut-Flower-Rose.jpg', messages: ['I’m a big fan of your art...'] },
-    { id: 3, name: 'Alex Johnson', avatar: 'https://img.kooora.com/?i=afp%2F20230829%2F20230829-afp_33tz3kl_afp.jpg', messages: ['i’d like to commission a piece from you'] },
-    { id: 4, name: 'Sophia Williams', avatar: 'https://example.com/avatar2.jpg', messages: ['Your art is amazing,keep up the great work!'] },
-    { id: 5, name: 'Daniel Miller', avatar: 'https://example.com/avatar2.jpg', messages: ['i’m a collector and i’d like to discuss purchasing your'] },
-
+    { id: 1, name: 'بسام جبور', avatar: 'https://www.palestineremembered.com/Jerusalem/GeoPoint/Dome_Of_The_Rock-10150.jpg', messages: ['Hey, I’m interested in your work'] },
+    { id: 2, name: 'ميار البزرة', avatar: 'https://image.made-in-china.com/2f0j00QWagThVImqzc/Birthday-Wedding-Party-Club-Promotion-Gift-Flower-Red-Fresh-Cut-Flower-Rose.jpg', messages: ['I’m a big fan of your art...'] },
+    { id: 3, name: 'احمد الحمامي', avatar: 'https://www.alquds.co.uk/wp-content/uploads/2023/10/06-10-730x438.jpg', messages: ['i’d like to commission a piece from you'] },
+    { id: 4, name: 'أسماء صايمة', avatar: 'https://meaningnames.net/write/19/%D9%86%D8%A7%D8%A8%D9%84%D8%B3.png', messages: ['Your art is amazing,keep up the great work!'] },
+    { id: 5, name: 'محي الدين حلاوة', avatar: 'https://ih1.redbubble.net/image.963338054.6243/flat,750x,075,f-pad,750x1000,f8f8f8.jpg', messages: ['i’m a collector and i’d like to discuss purchasing your'] },
   ]);
 
   const [activeUserId, setActiveUserId] = useState(null);
@@ -39,16 +38,15 @@ const Community = () => {
   const activeMessages = activeUser ? activeUser.messages : [];
 
   return (
-<div className="community">
-<div className="CommBody">
-      <UserListCommunity users={users} onUserClick={handleUserClick} />
-      <div className="main-content">
-        <ChatBoxCommunity activeUser={activeUser} messages={activeMessages} />
-        <WriteBoxCommunity activeUser={activeUser} onSendMessage={handleSendMessage} />
+    <div className="community">
+      <div className="CommBody">
+        <UserListCommunity users={users} onUserClick={handleUserClick} />
+        <div className="main-content">
+          <ChatBoxCommunity activeUser={activeUser} messages={activeMessages} />
+          <WriteBoxCommunity activeUser={activeUser} onSendMessage={handleSendMessage} />
+        </div>
       </div>
     </div>
-</div>
-
   );
 };
 
